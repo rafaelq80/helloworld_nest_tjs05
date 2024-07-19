d918d2319e5df6fdd703efcc31ecc0aa3819111c
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-07-19 11:10:47 -0300

    Hello World

---
+++ src/app.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getTurma(): string {
+    return '<i>Turma JavaScript 05</i>';
+  }
+
+}
